e553099cb85e8abbf61312ff963fc19992a1368c
victorm1510 <matheron.victor@gmail.com>
2025-03-30 14:59:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-mono">ENDRIP</h1>
+      <p className="text-xl mt-4">NEW COLLECTION</p>
+      <button className="mt-6 px-6 py-2 bg-gray-700 text-white rounded">Shop Now</button>
+    </div>
+  );
+}
